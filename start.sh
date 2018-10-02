82901920b86bda002f7a4f41a5520ebb779a43dd
yubin <yubin.cs05g@nctu.edu.tw>
2018-10-02 16:20:18 +0800

    Change install django-import-export method to pipenv

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-pipenv run pip install -e git+https://github.com/django-import-export/django-import-export.git#egg=django-import-export
 pipenv run python web60ss/manage.py makemigrations
 pipenv run python web60ss/manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')" | pipenv run python web60ss/manage.py shell
